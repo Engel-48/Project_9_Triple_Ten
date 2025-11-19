5f2cf4f680e4a7f448c2a9e2f49757614e6143e4
Engel-48 <engelpromise@gmail.com>
2025-11-19 16:28:05 -0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+urban_routes_url = 'https://cnt-9b6a73c8-7bdd-4b6e-bb6f-9832f5f8e2a3.containerhub.tripleten-services.com?lng=es'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number = '1234 5678 9100'
+card_code = '111'
+message_for_driver = "show museum's path"
+
